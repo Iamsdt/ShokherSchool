09021ce93b6a27ac2c6fbde33dbf645e7d09324c
Shudipto <Shudiptotrafder@gmail.com>
2018-01-01 17:28:12 +0600

    remove unused code

--- app/src/main/java/com/iamsdt/shokherschool/adapter/ClickListener.kt
+++ app/src/main/java/com/iamsdt/shokherschool/adapter/ClickListener.kt
@@ -1,11 +1,11 @@
 package com.iamsdt.shokherschool.adapter
 
-import com.iamsdt.shokherschool.viewModel.MainPostModelClass
+import com.iamsdt.shokherschool.model.PostModel
 
 /**
  * Created by Shudipto Trafder on 11/24/2017.
  * at 4:42 PM
  */
 interface ClickListener {
-    fun onPostItemClick(post: MainPostModelClass)
+    fun onPostItemClick(post: PostModel)
 }
