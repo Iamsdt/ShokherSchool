9aee235638372671886531bd29e5e4a8e221dba2
Shudipto <Shudiptotrafder@gmail.com>
2018-01-07 00:36:49 +0600

    change dagger

--- app/src/main/java/com/iamsdt/shokherschool/injection/module/FragmentModule.kt
+++ app/src/main/java/com/iamsdt/shokherschool/injection/module/FragmentModule.kt
@@ -14,6 +14,6 @@ class FragmentModule(private val baseFragment: BaseFragment){
 
     @Provides
     @ServicesScope
-    fun provideFargment(): BaseFragment
+    fun provideFragment(): BaseFragment
             = baseFragment
 }
