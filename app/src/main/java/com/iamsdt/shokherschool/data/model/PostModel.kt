d5733369f1354461daf21abc3b83ce5974f726cc
Shudipto <Shudiptotrafder@gmail.com>
2018-01-12 18:48:26 +0600

    add bookmark

--- app/src/main/java/com/iamsdt/shokherschool/data/model/PostModel.kt
+++ app/src/main/java/com/iamsdt/shokherschool/data/model/PostModel.kt
@@ -4,8 +4,9 @@ package com.iamsdt.shokherschool.data.model
  * Created by Shudipto Trafder on 12/30/2017.
  * at 4:08 PM
  */
-class PostModel(var id: Int? = 0,
+class PostModel(var id: Int = 0,
                 var date: String? = "",
                 var title: String? = "",
                 var author: String? = "",
-                var mediaLink: String? = "")
+                var mediaLink: String? = "",
+                var bookmark:Int = 0)
