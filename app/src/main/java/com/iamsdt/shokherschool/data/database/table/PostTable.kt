cc4c924d3555a9a303330c7668e6d1c6b5e34e5c
Shudipto <Shudiptotrafder@gmail.com>
2018-01-12 18:53:15 +0600

    add bookmark

--- app/src/main/java/com/iamsdt/shokherschool/data/database/table/PostTable.kt
+++ app/src/main/java/com/iamsdt/shokherschool/data/database/table/PostTable.kt
@@ -23,6 +23,6 @@ class PostTable(@PrimaryKey
                 var postCategories:String ?= "",
                 var tags:String ?= "",
                 var commentStatus:String ?= "",
-                var bookmark:Int = 0, //0 for false
+                var bookmark:Int = 0, //0 for false as deafult
                 @Embedded
                 var mediaTable:MediaTable ?= MediaTable())
