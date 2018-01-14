f3b6289e064e33ccd7f3ce3d4f4c51d2c387a43d
Shudipto <Shudiptotrafder@gmail.com>
2018-01-15 00:27:34 +0600

    settings

--- app/src/main/java/com/iamsdt/shokherschool/ui/activity/SettingsActivity.kt
+++ app/src/main/java/com/iamsdt/shokherschool/ui/activity/SettingsActivity.kt
@@ -2,12 +2,31 @@ package com.iamsdt.shokherschool.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.ftinc.scoop.Scoop
 import com.iamsdt.shokherschool.R
+import kotlinx.android.synthetic.main.activity_settings.*
+
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Scoop.getInstance().apply(this)
+
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
